a79a82a01a984f63dc577d3ceb9181e25f4c53e1
Zuzanna Zajkiewicz <machefi1@gmail.com>
2021-08-12 22:59:20 +0200

    Add Flask-RESTful

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    return "Hello world"
+class Student(Resource):
 
+    def get(self, name):
+        return {'student': name}
+
+api.add_resource(Student, '/student/<string:name>')
 
 app.run(port=5000)
